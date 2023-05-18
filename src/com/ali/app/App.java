6ad9419643813e13a4610f2c56ba6e69b62cb56e
mehmet cetinkol <alimehmet.cetinkol@gmail.com>
2023-05-18 20:37:29 +0300

    this is a crap game.

--- src/com/ali/app/App.java
+++ src/com/ali/app/App.java
@@ -1,4 +1,39 @@
 package com.ali.app;
 
 public class App {
+    public static void main(String[] args) {
+        int trials = 100000;
+        int wins = 0;
+
+        for (int t = 0; t < trials; t++)
+            if (Craps.winsPassBet()) wins++;
+        System.out.println("Winning percentage = " + 1.0 * wins / trials);
+    }
 }
+class Craps{
+    public static int uniformInt(int n) {
+        return (int) (Math.random() * n);
+    }
+    public static int sumOfTwoDice() {
+        int x = 1 + uniformInt(6);
+        int y = 1 + uniformInt(6);
+        return x + y;
+    }
+    public static boolean winsPassBet() {
+        int x = sumOfTwoDice();
+        if (x == 7 || x == 11)
+            return true;
+        if (x == 2 || x == 3 || x == 12)
+            return false;
+
+        while (true) {
+            int y = sumOfTwoDice();
+            if (y == 7)
+                return false;
+            if (y == x)
+                return true;
+        }
+    }
+}
+
+
